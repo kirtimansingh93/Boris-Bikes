37745d453a701bfc0bd8d8d355d9cac5b2fa749b
Kirtiman Singh <kirt93@hotmail.com>
2018-10-16 19:28:13 +0100

    failing tests - docking station responds to bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,9 +7,13 @@ describe DockingStation do
     expect(bike).to be_working
   end
 
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
   it 'docks a bike in the docking station' do
     bike = Bike.new
     expect(subject.dock(bike)).to eq(bike)
   end
+
+  it { is_expected.to respond_to(:bike) }
 
 end
