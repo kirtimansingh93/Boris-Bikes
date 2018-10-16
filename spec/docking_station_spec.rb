67ca12fe4d5afc520b6ac44df48db03efb79ffe2
Kirtiman Singh <kirt93@hotmail.com>
2018-10-16 20:49:41 +0100

    changed test names and added failing test for #release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,21 +2,29 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-  it 'releases a bike that works' do
-    bike = subject.release_bike
+  it '#release_bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.release_bike).to eq bike
+  end
+
+  it '#working?' do
+    bike = Bike.new
+    subject.dock(bike)
+    subject.release_bike
     expect(bike).to be_working
   end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
-  it 'docks a bike in the docking station' do
+  it '#dock(bike)' do
     bike = Bike.new
     expect(subject.dock(bike)).to eq(bike)
   end
 
   it { is_expected.to respond_to(:bike) }
 
-  it 'checks whether there is a bike to return' do
+  it '#bike' do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq(bike)
