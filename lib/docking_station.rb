75dbaae2d2e232a42c5b6a97f419a63af8697675
Kirtiman Singh <kirt93@hotmail.com>
2018-10-16 21:25:01 +0100

    test passes for #release_bike: raises an error when there are no more bikes available

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,11 +3,14 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
+    raise 'No bikes available.' unless @bike
     @bike
+  # If bike = Bike.new hasn't been docked, then @bike instance variable contains nothing.
   end
 
   def dock(bike)
     @bike = bike
+  # This saves bike = Bike.new into @bike. Now the dockingstation has a bike to release.
   end
 
 end
